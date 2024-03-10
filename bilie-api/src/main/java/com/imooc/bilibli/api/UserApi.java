78bdb2bbeaa7decbc9778504b062f1c6ef5bf835
lyk990 <1477435414@qq.com>
2024-03-10 21:57:33 +0800

    restful 接口

---
+++ bilie-api/src/main/java/com/imooc/bilibli/api/UserApi.java
@@ -0,0 +1,22 @@
+package com.imooc.bilibli.api;
+
+import com.imooc.bilibili.domain.JsonResponse;
+import com.imooc.bilibili.service.UserService;
+import com.imooc.bilibili.service.util.RSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApi {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/rsa-pks")
+    public JsonResponse<String> getRsaPublickKey() {
+        String pk = RSAUtil.getPublicKeyStr();
+        return new JsonResponse<>(pk);
+
+    }
+}
