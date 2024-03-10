cbc78524470cd4bb868f56b024c8288b623fb366
lyk990 <1477435414@qq.com>
2024-03-10 22:29:18 +0800

    checkUser

--- bilie-api/src/main/java/com/imooc/bilibli/api/UserApi.java
+++ bilie-api/src/main/java/com/imooc/bilibli/api/UserApi.java
@@ -1,10 +1,13 @@
 package com.imooc.bilibli.api;
 
 import com.imooc.bilibili.domain.JsonResponse;
+import com.imooc.bilibili.domain.User;
 import com.imooc.bilibili.service.UserService;
 import com.imooc.bilibili.service.util.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +21,10 @@ public class UserApi {
         String pk = RSAUtil.getPublicKeyStr();
         return new JsonResponse<>(pk);
 
+    }
+
+    @PostMapping("/users")
+    public JsonResponse<String> addUser(@RequestBody User user) {
+        userService.addUser(user);
     }
 }
--- bilie-service/src/main/java/com/imooc/bilibili/service/UserService.java
+++ bilie-service/src/main/java/com/imooc/bilibili/service/UserService.java
@@ -1,12 +1,27 @@
 package com.imooc.bilibili.service;
 
 import com.imooc.bilibili.dao.UserDao;
+import com.imooc.bilibili.domain.User;
+import com.imooc.bilibili.domain.exception.ConditionalException;
+import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.locks.Condition;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserDao userDao;
+
+
+    public void addUser(User user) {
+        String phone = user.getPhone();
+
+        if(StringUtils.isNullOrEmpty(phone)){
+            throw new ConditionalException("手机号不能为空");
+        }
+    }
+
 }
