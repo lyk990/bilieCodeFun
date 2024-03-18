f8dcee731f9d85d42b08bb3520ba276f26213ddb
lyk990 <1477435414@qq.com>
2024-03-18 17:21:30 +0800

    创建微服务文件

---
+++ gulimail-coupon/src/test/java/com/aiguigu/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiguigu.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
