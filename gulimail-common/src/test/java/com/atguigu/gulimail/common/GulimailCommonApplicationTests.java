c70d3847d1a8c3ad70290c6a2e8c68f0e2d0ff25
lyk990 <1477435414@qq.com>
2024-03-20 21:48:13 +0800

    guli-common

---
+++ gulimail-common/src/test/java/com/atguigu/gulimail/common/GulimailCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
