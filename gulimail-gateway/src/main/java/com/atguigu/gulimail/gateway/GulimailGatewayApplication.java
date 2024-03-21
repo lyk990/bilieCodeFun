56efcf6ae9c287ce2136bf043207e867cb7a963c
lyk990 <1477435414@qq.com>
2024-03-21 22:17:20 +0800

    网关

---
+++ gulimail-gateway/src/main/java/com/atguigu/gulimail/gateway/GulimailGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailGatewayApplication.class, args);
+    }
+
+}
