9a77ef82184cd77c026133dddc7b8d5a6e0818c6
lyk990 <1477435414@qq.com>
2024-04-07 17:41:18 +0800

    路由转发

--- course/system/src/main/java/com/course/system/controller/TestController.java
+++ course/system/src/main/java/com/course/system/controller/TestController.java
@@ -2,7 +2,6 @@ package com.course.system.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class TestController {
 
