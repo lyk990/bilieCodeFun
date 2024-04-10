64894b4d2028ce09b9fb40fba7541945601c4d8b
lyk990 <1477435414@qq.com>
2024-04-10 22:27:33 +0800

    ComponentScan

--- course/system/src/main/java/com/course/system/controller/TestController.java
+++ course/system/src/main/java/com/course/system/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.course.system.controller;
 
-import com.course.system.domain.Test;
-import com.course.system.service.TestService;
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,6 @@ public class TestController {
 
     @Resource
     private TestService testService;
-
     @RequestMapping("/test")
     public List<Test> test() {
         return testService.list();
